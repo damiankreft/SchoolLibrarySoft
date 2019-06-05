d16b064e068c32934cdd5d9fa29898e8e58055b4
Damian Kreft <damiankreft87@gmail.com>
2019-06-06 00:18:40 +0200

    Implemented proper releaseYear validation

--- SchoolLibrary/ViewModel/AddBookViewModel.cs
+++ SchoolLibrary/ViewModel/AddBookViewModel.cs
@@ -1,12 +1,28 @@
 ﻿using SchoolLibrary.Models;
+using System.Text.RegularExpressions;
 
 namespace SchoolLibrary.ViewModel
 {
     static class AddBookViewModel
     {
-        public static void Add(string author, bool availability, string publisher, string title, int releaseYear)
+        public static void Add(string author, bool availability, string publisher, string title, string releaseYear)
         {
-            Database.Database.Instance.SaveItemAsync(new BookModel() { Author = author, IsAvailable = availability, Publisher = publisher, Title = title, ReleaseYear = releaseYear});
+            if (IsReleaseYearValid(releaseYear))
+            {
+                Database.Database.Instance.SaveItemAsync(new BookModel() { Author = author, IsAvailable = availability, Publisher = publisher, Title = title, ReleaseYear = System.Int32.Parse(releaseYear) });
+            }
+            else
+                System.Console.WriteLine("Invalid data");
+        }
+
+        private static bool IsReleaseYearValid(string releaseYear)
+        {
+            if (new Regex(@"^[0-9]+$").IsMatch(releaseYear))
+            {
+                return true;
+            }
+            else
+                return false;
         }
     }
 }
